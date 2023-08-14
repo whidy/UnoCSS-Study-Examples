f5e8ce15644ea2e2568fd07b19e944921978e9ee
baishun <baishun@wodcloud.com>
2023-08-14 17:11:15 +0800

    完善icon

--- src/components/VMenu.vue
+++ src/components/VMenu.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- "i-carbon-book group-hover:i-carbon-notebook-reference i-carbon-ibm-toolchain i-carbon-asset i-carbon-code i-carbon-face-wink group-hover:i-carbon-face-satisfied i-carbon-group-objects i-carbon-at i-carbon-tree-view-alt i-carbon-inspection i-carbon-face-dizzy-filled group-hover:i-carbon-user-favorite" -->
   <el-menu
     :default-active="activedItem"
     :class="menuType === 'drawer' && 'p-0'"
@@ -8,15 +9,12 @@
     <el-menu-item
       v-for="item in menu"
       :key="item.name"
-      ref="itemRefs"
       class="group flex items-center text-sm decoration-none space-x-2"
       :index="item.name"
       :route="item">
-      <!-- 这段注释掉的的代码，虽然可以生成相同的代码，但是并不能显示icon -->
-      <!-- <i
+      <i
         class="text-dark dark:text-white hover:text-cyan"
-        :class="item.icon + ' group-hover:' + item.hoverIcon"></i><span text-dark dark:text-white>{{ item.topic }}</span> -->
-      <i class="text-dark dark:text-white hover:text-cyan"></i><span text-dark dark:text-white>{{ item.topic }}</span>
+        :class="item.icon + ' group-hover:' + item.hoverIcon"></i><span text-dark dark:text-white>{{ item.topic }}</span>
     </el-menu-item>
   </el-menu>
 </template>
@@ -60,8 +58,6 @@ watch(router.currentRoute, (currentRoute) => {
   activedItem.value = currentRoute.name as string;
 });
 
-const itemRefs: Ref = ref([]);
-
 const menu = computed(() => {
   const routeList = router.getRoutes().filter((item) => item.meta.menuShow);
   return routeList.map((item) => {
@@ -80,31 +76,6 @@ const handleSelect = (index: string) => {
   router.push(`/${index}`);
 };
 
-// const iconShowsDelay = async(routeItem: {
-//   name?: RouteRecordName | undefined;
-//   path?: string;
-//   icon?: any;
-//   hoverIcon?: any;
-// }) => {
-//   const result = await new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       // console.log(routeItem.icon);
-//       resolve(`${routeItem.icon} group-hover:${routeItem.hoverIcon}`);
-//     }, 1000);
-//   });
-//   return result;
-// };
-
-onMounted(() => {
-  // 这个方案虽然不是很好，但是似乎目前也只能这样了？ --- 糟糕，开发环境下有效，生产环境无效。
-  itemRefs.value.forEach((item: RefItemOfMenu) => {
-    const iconElement = item.$el!.querySelector("i");
-    iconElement!.classList.add(
-      item.route.icon,
-      `group-hover:${item.route.hoverIcon}`,
-    );
-  });
-});
 </script>
 
 <style scoped>
