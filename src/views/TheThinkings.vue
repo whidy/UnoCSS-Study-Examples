51aa4a77372c17d5374f5b77eb7c46483859aec0
baishun <baishun@wodcloud.com>
2023-08-22 09:30:25 +0800

    修正动态加载的类名的示例

--- src/views/TheThinkings.vue
+++ src/views/TheThinkings.vue
@@ -7,13 +7,16 @@
       <el-card>
         <template #header> 动态加载的类名 </template>
         <ul
-          v-for="(i, index) in rankTextColorList"
+          v-for="(name, index) in rankPeople"
           :key="index"
           class="list-none pl-0">
           <!-- #1 don't use this -->
           <!-- <li>{{ index + 1 }}. <span :class="comp(index)">I Love Whidy</span></li> -->
           <!-- use this -->
-          <li>{{ index + 1 }}. <span :class="i">I Love Whidy</span></li>
+          <li>
+            {{ index + 1 }}.
+            <span :class="rankTextColorList[index]">{{ name }}</span>
+          </li>
         </ul>
       </el-card>
     </template>
@@ -25,12 +28,13 @@ import markdownContent from "./contents/Thinkings.md";
 // #1 don't use this
 const comp = (index: number) => `text-red-${index + 1}00`;
 // #2 use this
+const rankPeople = ["Whidy", "Tom", "Lucy", "Teddy", "Somebody"];
 const rankTextColorList = [
-  "text-red-100",
-  "text-red-200",
-  "text-red-300",
-  "text-red-400",
   "text-red-500",
+  "text-red-400",
+  "text-red-300",
+  "text-red-200",
+  "text-red-100",
 ];
 </script>
 
